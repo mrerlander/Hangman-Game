a318f3beeaf79dd9d47efcd5cfb53b4737c7d99b
Michael@MIKE-LAPTOP <mrerlander@gmail.com>
2018-02-01 15:15:47 -0600

    Base html and pseudo code for js

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+//create global variables (computer word choices array, userguesses array correct guess array, wins, losses, attempts)
+
+//computer chooses a word
+//computer writes correct number of blanks to page (for loop for word length)
+
+//function for keyup
+//user guess = event.key
+//convert caps to lowercase
+
+//if user guesses a letter
+    //function for allTrue
+    //function for anytrue
+
+    //if anyTrue write letter to correct space
+    //else if allTrue run win screen
+        
+    //else replace hangman image with next image attempts--
+
+//if else for duplicate letter
+//else for non-letter
